67d6b7585e4bbfe41ddb451982f4519d27896f7e
Jose <alostnight1993@outlook.com>
2020-03-07 19:42:03 -0600

    base done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 import PhotoofDay from "./components/PhotoofDay";
 import NASA_seal from "./assets/NASA_seal.png"
 
@@ -13,8 +14,6 @@ function App() {
       <div className="logo_container">
         <img className="logo" src={NASA_seal} alt="Nasa official seal" />
         <h1>Nasa Photo of the Day</h1>
-
-        
       </div>
       <PhotoofDay />
       </p>
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -4,13 +4,11 @@ import PhotoofDay from "./PhotoofDay.js";
 const PhotoCard = props => {
   return (
     <li className="photo-card" key={props.id}>
-      <h2>Photo title: {props.title}</h2>
+      <p>Date: {props.date}</p>
       <img src= {props.photo} alt= "Photo_of_Day"/>
+      <h2>Photo title: {props.title}</h2>
+      <p>{props.description}</p>
       <p>Author: {props.author}</p>
-      <p>{props.explanation}</p>
-      <p>
-        Date: {props.date}
-      </p>
     </li>
   );
 };
